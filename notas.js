6c4b5584424a66684946a8a9da37aefb89d1db32
Marcela Costa <marcela.costa@sou.inteli.edu.br>
2025-02-10 11:53:40 -0300

    correção da menor nota

--- notas.js
+++ notas.js
@@ -1,6 +1,6 @@
 var nota1 = 5;
 var nota2 = 8;
-var nota3 = 2;
+var nota3 = 9.5;
 
 var media = (nota1 + nota2 + nota3) /3;
 
@@ -23,11 +23,10 @@ maior = nota1;
 var i = 1;
 
 while(i <= 3){
-    if(i == 1){
-        if(nota2 > maior){
-            maior = nota2;
-        }
-    } else if(i == 2){
+   if(nota2 > maior){
+        maior = nota2;
+    }
+     else {
         if(nota3 > maior){
             maior = nota3;
         }
@@ -40,11 +39,10 @@ menor = nota1;
 var n = 1;
 
 while(n <= 3){
-    if(n == 1){
-        if(nota2 < menor){
-            menor = nota2;
-        }
-    } else if(i == 2){
+   if(nota2 < menor){
+        menor = nota2;
+    }
+    else{
         if(nota3 < menor){
             menor = nota3;
         }
